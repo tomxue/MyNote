dc31dcf2ac4e95b78d7233cd00c5e2ea8482a77a
Tom Xue <tomxue0126@gmail.com>
2012-08-05 13:46:30 +0800

    tidy design; try to add Popup menu functionality but failed

--- src/my/demo/MyNoteUI.java
+++ src/my/demo/MyNoteUI.java
@@ -3,14 +3,13 @@
  * and open the template in the editor.
  */
 package my.demo;
-import java.awt.*;
+
 import java.awt.event.*;
-import javax.swing.*;
 import java.io.*;
-import java.util.*;
-import java.nio.channels.FileChannel;	
 import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
 import java.nio.charset.Charset;
+import javax.swing.*;
 
 /**
  *
@@ -18,26 +17,31 @@ import java.nio.charset.Charset;
  */
 public class MyNoteUI extends javax.swing.JFrame {
 
-	JFrame jframe;
+	JFrame jFrame;
 	JFileChooser fc;
 
 	/**
 	 * Creates new form MyNoteUI
 	 */
 	public MyNoteUI() {
+		
 		initComponents();
+
+		jFrame = new javax.swing.JFrame();
+		jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		jFrame.setContentPane(new PopupMenuExample());
+		jFrame.setTitle("My NoteUI");
+
 	}
 	
 	public String readFile(String path) throws IOException {
 		System.out.println("The opened file path is: "+path);
-		FileInputStream stream = new FileInputStream(new File(path));
-		try {
-			FileChannel fc = stream.getChannel();
-			MappedByteBuffer bb = fc.map(FileChannel.MapMode.READ_ONLY, 0, fc.size());
+		try (FileInputStream stream = new FileInputStream(new File(path))) {
+			FileChannel filechannel;
+			filechannel = stream.getChannel();
+			MappedByteBuffer bb = filechannel.map(FileChannel.MapMode.READ_ONLY, 0, filechannel.size());
 			/* Instead of using default, pass in a decoder. */
 			return Charset.defaultCharset().decode(bb).toString();
-		} finally {
-			stream.close();
 		}
 	}
 	
@@ -54,11 +58,13 @@ public class MyNoteUI extends javax.swing.JFrame {
 			this.frame = frame;
 		}
 
+		@Override
 		public void actionPerformed(ActionEvent evt) {
 			// Show dialog; this method does not return until dialog is closed
 			chooser.showOpenDialog(frame);
 
 			// Get the selected file
+			
 			File file = chooser.getSelectedFile();
 			//jTextArea1.setText("The opended file path is: "+file.toString());
 
@@ -82,6 +88,7 @@ public class MyNoteUI extends javax.swing.JFrame {
 			this.frame = frame;
 		}
 
+		@Override
 		public void actionPerformed(ActionEvent evt) {
 
 			// Get the selected file
@@ -91,10 +98,9 @@ public class MyNoteUI extends javax.swing.JFrame {
 				// Create file 
 				System.out.println("The file path is: "+file.toString());
 				FileWriter fstream = new FileWriter(file.toString());
-				BufferedWriter out = new BufferedWriter(fstream);
-				out.write(jTextArea1.getText());
-				//Close the output stream
-				out.close();
+				try (BufferedWriter out = new BufferedWriter(fstream)) {
+					out.write(jTextArea1.getText());
+				}
 			} catch (Exception e) {//Catch exception if any
 				System.err.println("Error: " + e.getMessage());
 			}	
@@ -113,6 +119,7 @@ public class MyNoteUI extends javax.swing.JFrame {
 			this.frame = frame;
 		}
 
+		@Override
 		public void actionPerformed(ActionEvent evt) {
 			// Show dialog; this method does not return until dialog is closed
 			chooser.showSaveDialog(frame);
@@ -124,10 +131,9 @@ public class MyNoteUI extends javax.swing.JFrame {
 				// Create file 
 				System.out.println("The file path is: "+file.toString());
 				FileWriter fstream = new FileWriter(file.toString());
-				BufferedWriter out = new BufferedWriter(fstream);
-				out.write(jTextArea1.getText());
-				//Close the output stream
-				out.close();
+				try (BufferedWriter out = new BufferedWriter(fstream)) {
+					out.write(jTextArea1.getText());
+				}
 			} catch (Exception e) {//Catch exception if any
 				System.err.println("Error: " + e.getMessage());
 			}	
@@ -151,6 +157,7 @@ public class MyNoteUI extends javax.swing.JFrame {
                 jButton2 = new javax.swing.JButton();
                 jButton3 = new javax.swing.JButton();
                 jButton4 = new javax.swing.JButton();
+                jButton5 = new javax.swing.JButton();
 
                 setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -186,6 +193,13 @@ public class MyNoteUI extends javax.swing.JFrame {
                         }
                 });
 
+                jButton5.setText("Help");
+                jButton5.addActionListener(new java.awt.event.ActionListener() {
+                        public void actionPerformed(java.awt.event.ActionEvent evt) {
+                                jButton5ActionPerformed(evt);
+                        }
+                });
+
                 javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
                 jPanel1.setLayout(jPanel1Layout);
                 jPanel1Layout.setHorizontalGroup(
@@ -202,6 +216,8 @@ public class MyNoteUI extends javax.swing.JFrame {
                                                 .addComponent(jButton3)
                                                 .addGap(29, 29, 29)
                                                 .addComponent(jButton2)
+                                                .addGap(33, 33, 33)
+                                                .addComponent(jButton5)
                                                 .addGap(0, 0, Short.MAX_VALUE)))
                                 .addContainerGap())
                 );
@@ -209,11 +225,12 @@ public class MyNoteUI extends javax.swing.JFrame {
                         jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                         .addGroup(jPanel1Layout.createSequentialGroup()
                                 .addContainerGap()
-                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.CENTER)
                                         .addComponent(jButton1)
-                                        .addComponent(jButton2)
+                                        .addComponent(jButton4)
                                         .addComponent(jButton3)
-                                        .addComponent(jButton4))
+                                        .addComponent(jButton2)
+                                        .addComponent(jButton5))
                                 .addGap(18, 18, 18)
                                 .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 348, Short.MAX_VALUE)
                                 .addContainerGap())
@@ -233,6 +250,7 @@ public class MyNoteUI extends javax.swing.JFrame {
                 pack();
         }// </editor-fold>//GEN-END:initComponents
 
+
         private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
 		System.exit(0);
         }//GEN-LAST:event_jButton2ActionPerformed
@@ -245,9 +263,10 @@ public class MyNoteUI extends javax.swing.JFrame {
 		// Create a file chooser
 		String filename = File.separator + "home/tomxue";	// the default opened path
 		fc = new JFileChooser(new File(filename));
+		JFrame myframe = null;
 
 		// Create the actions
-		Action openAction = new OpenFileAction(jframe, fc);
+		Action openAction = new OpenFileAction(myframe, fc);
 		openAction.actionPerformed(evt);
 		
         }//GEN-LAST:event_jButton1ActionPerformed
@@ -260,9 +279,10 @@ public class MyNoteUI extends javax.swing.JFrame {
 		// Create a file chooser
 		String filename = File.separator + "home/tomxue";	// the default opened path
 		fc = new JFileChooser(new File(filename));
+		JFrame myframe = null;
 
 		// Create the actions
-		Action saveAction = new SaveAsFileAction(jframe, fc);
+		Action saveAction = new SaveAsFileAction(myframe, fc);
 		saveAction.actionPerformed(evt);
         }//GEN-LAST:event_jButton3ActionPerformed
 
@@ -271,16 +291,21 @@ public class MyNoteUI extends javax.swing.JFrame {
                 /// Create a file chooser
 		String filename = File.separator + "home/tomxue";	// the default opened path
 		//fc = new JFileChooser(new File(filename));
+		JFrame myframe = null;
 
 		// Create the actions
-		Action saveAction = new SaveFileAction(jframe, fc);
+		Action saveAction = new SaveFileAction(myframe, fc);
 		saveAction.actionPerformed(evt);
         }//GEN-LAST:event_jButton4ActionPerformed
+
+        private void jButton5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton5ActionPerformed
+        }//GEN-LAST:event_jButton5ActionPerformed
 
 	/**
 	 * @param args the command line arguments
 	 */
 	public static void main(String args[]) {
+		
 		/* Set the Nimbus look and feel */
 		//<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
@@ -293,29 +318,27 @@ public class MyNoteUI extends javax.swing.JFrame {
 					break;
 				}
 			}
-		} catch (ClassNotFoundException ex) {
-			java.util.logging.Logger.getLogger(MyNoteUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-		} catch (InstantiationException ex) {
-			java.util.logging.Logger.getLogger(MyNoteUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-		} catch (IllegalAccessException ex) {
-			java.util.logging.Logger.getLogger(MyNoteUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-		} catch (javax.swing.UnsupportedLookAndFeelException ex) {
+		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
 			java.util.logging.Logger.getLogger(MyNoteUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
 		}
 		//</editor-fold>
 
 		/* Create and display the form */
 		java.awt.EventQueue.invokeLater(new Runnable() {
+			@Override
 			public void run() {
 				new MyNoteUI().setVisible(true);
+
 			}
 		});
+
 	}
         // Variables declaration - do not modify//GEN-BEGIN:variables
         private javax.swing.JButton jButton1;
         private javax.swing.JButton jButton2;
         private javax.swing.JButton jButton3;
         private javax.swing.JButton jButton4;
+        private javax.swing.JButton jButton5;
         private javax.swing.JPanel jPanel1;
         private javax.swing.JScrollPane jScrollPane1;
         private javax.swing.JTextArea jTextArea1;
